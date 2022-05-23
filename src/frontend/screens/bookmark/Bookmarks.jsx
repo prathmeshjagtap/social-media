cb42109b9b70051f2444393ae71002e9829520c6
prathmesh <prathmeshjagtap405@gmail.com>
2022-05-23 20:00:34 +0530

    fixed liked issue

--- src/frontend/screens/bookmark/Bookmarks.jsx
+++ src/frontend/screens/bookmark/Bookmarks.jsx
@@ -5,21 +5,26 @@ import { Posts, Loader } from "../../components";
 import { Box, Text } from "@chakra-ui/react";
 
 function Bookmarks() {
-	const { bookmarks, bookmarksStatus } = useSelector((state) => state.posts);
+	const { bookmarks, bookmarksStatus, posts } = useSelector(
+		(state) => state.posts
+	);
 	const { token } = useSelector((state) => state.auth);
+
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(getBookmarks(token));
-
 		return () => dispatch(unsuscribeBookmark());
 	}, [token, dispatch]);
 
+	const bookmarkPosts = posts.filter((post) =>
+		bookmarks.find((item) => post._id === item._id)
+	);
 	return (
 		<Box>
 			{bookmarksStatus === "loading" && <Loader />}
 			{bookmarksStatus === "success" ? (
 				bookmarks?.length > 0 ? (
-					bookmarks?.map((post) => <Posts key={post.id} post={post} />)
+					bookmarkPosts.map((post) => <Posts key={post.id} post={post} />)
 				) : (
 					<Text mt="5" textAlign="center" p="4">
 						No Bookmarked Posts
--- src/frontend/screens/singlePost/SinglePost.jsx
+++ src/frontend/screens/singlePost/SinglePost.jsx
@@ -1,30 +1,15 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getSinglePost, unsuscribeSinglePost } from "../../features";
-import { Loader, Posts } from "../../components";
-import { Text, Box } from "@chakra-ui/react";
+import { Posts } from "../../components";
+import { Box } from "@chakra-ui/react";
 function SinglePost() {
 	const { postId } = useParams();
-	const dispatch = useDispatch();
-	const { singlePost, singlePostStatus } = useSelector((state) => state.posts);
-	useEffect(() => {
-		dispatch(getSinglePost(postId));
+	const { posts } = useSelector((state) => state.posts);
 
-		return () => dispatch(unsuscribeSinglePost());
-	}, [postId, dispatch]);
+	const updatedSinglepost = posts.filter((item) => item?._id === postId);
 
-	return (
-		<Box>
-			{singlePostStatus === "loading" && <Loader />}
-			{singlePostStatus === "success" && <Posts post={singlePost} />}
-			{singlePostStatus === "failed" && (
-				<Text mt="5" textAlign="center" color="red.500" p="4">
-					Error Failed to load The data
-				</Text>
-			)}
-		</Box>
-	);
+	return <Box>{<Posts post={updatedSinglepost[0]} />}</Box>;
 }
 
 export { SinglePost };
