c00c62ec556869760655af5c9e5d67713a3b175f
prathmesh <prathmeshjagtap405@gmail.com>
2022-07-04 22:37:30 +0530

    unfollow fix

--- src/backend/controllers/UserController.js
+++ src/backend/controllers/UserController.js
@@ -272,8 +272,9 @@ export const unfollowUserHandler = function (schema, request) {
 				}
 			);
 		}
+
 		const isFollowing = user.following.some(
-			(currUser) => currUser._id === followUser._id
+			(currUser) => currUser.username === followUser.username
 		);
 
 		if (!isFollowing) {
@@ -283,13 +284,13 @@ export const unfollowUserHandler = function (schema, request) {
 		const updatedUser = {
 			...user,
 			following: user.following.filter(
-				(currUser) => currUser._id !== followUser._id
+				(currUser) => currUser.username !== followUser.username
 			),
 		};
 		const updatedFollowUser = {
 			...followUser,
 			followers: followUser.followers.filter(
-				(currUser) => currUser._id !== user._id
+				(currUser) => currUser.username !== user.username
 			),
 		};
 		this.db.users.update(
@@ -300,7 +301,6 @@ export const unfollowUserHandler = function (schema, request) {
 			{ _id: followUser._id },
 			{ ...updatedFollowUser, updatedAt: formatDate() }
 		);
-
 		return new Response(200, {}, { users: this.db.users });
 	} catch (error) {
 		return new Response(
